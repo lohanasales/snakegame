b3cc8620504a0e078a36064faea331649f75ad09
Lohana <lohanasales@hotmail.com>
2020-12-28 10:23:06 -0400

    alterações

---
+++ script.js
@@ -0,0 +1,25 @@
+let canvas = document.createElement("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+};
+
+let direction = "right";
+
+function criarBG () {
+    context.fillStyle = "lightgreen";
+    context.fillReact(0, 16 * box, 16 * box);
+}
+
+function criarCobrinha(){
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillReact(snake[i].x, snake[i].y, box, box);
+    } 
+}
+
+criarBG ();
+criarCobrinha();
